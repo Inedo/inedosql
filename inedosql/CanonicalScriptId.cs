c4590c93de6989b83c462311980e38e133aa60b1
Alex Papadimoulis, Inedo <apapadimoulis@inedo.com>
2021-01-05 15:31:15 +0900

    Added UseTransaction option to the additional header statement and defaulted to true

--- inedosql/CanonicalScriptId.cs
+++ inedosql/CanonicalScriptId.cs
@@ -4,15 +4,17 @@ namespace Inedo.DbUpdater
 {
     internal readonly struct CanonicalScriptId
     {
-        public CanonicalScriptId(Guid guid, int? legacyId, ExecutionMode mode)
+        public CanonicalScriptId(Guid guid, int? legacyId, ExecutionMode mode, bool tran)
         {
             this.Guid = guid;
             this.LegacyId = legacyId;
             this.Mode = mode;
+            this.UseTransaction = tran;
         }
 
         public Guid Guid { get; }
         public int? LegacyId { get; }
         public ExecutionMode Mode { get; }
+        public bool UseTransaction { get; }
     }
 }
