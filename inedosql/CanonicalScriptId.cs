4bc70abc88634b2d3d0abfb139ef3a253c6ecd19
Greg D <gdivis@inedo.com>
2021-01-05 10:56:06 -0500

    renamed a few things

--- inedosql/CanonicalScriptId.cs
+++ inedosql/CanonicalScriptId.cs
@@ -4,12 +4,12 @@ namespace Inedo.DbUpdater
 {
     internal readonly struct CanonicalScriptId
     {
-        public CanonicalScriptId(Guid guid, int? legacyId, ExecutionMode mode, bool tran)
+        public CanonicalScriptId(Guid guid, int? legacyId, ExecutionMode mode, bool useTransaction)
         {
             this.Guid = guid;
             this.LegacyId = legacyId;
             this.Mode = mode;
-            this.UseTransaction = tran;
+            this.UseTransaction = useTransaction;
         }
 
         public Guid Guid { get; }
